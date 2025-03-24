8e784087fcbe430006f9d96c25621f2ddb2958da
Andrey Celestino <lab53@fatec.com>
2025-03-24 09:45:22 -0300

    1.0

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+Console.Clear();
+
+/*
+8% de salario
+
+salario = 100
+fgts    =  8
+*/
+
+float salario;
+float fgts;
+
+Console.WriteLine("Digite o salário:");
+salario = Convert.ToSingle(Console.ReadLine());
+
+fgts = (salario * 8) / 100;
+
+Console.WriteLine($"FGTS: R${Math.Round(fgts, 2)}");
